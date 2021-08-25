f51d4f9302e85040ea4879e8b8d9fea60c9b1945
mjz19910 <mjz19910@users.noreply.github.com>
2021-08-25 07:01:05 -0700

    rsToJS:start matching rust

--- src/main.rs_to_js.js
+++ src/main.rs_to_js.js
@@ -1,1 +1,8 @@
-
+let rust_code=`fn main() {
+    println!("Hello World");
+}`;
+let rust_match_rx=/_(?=!>[a-zA-Z_])|fn|self|match|yield|macro|impl|\s|[\(\)]|[\[\]]|[{}]|::|->|<-|@|[a-zA-Z_]/;
+let cidx=0;
+let mt=rust_match_rx.exec(rust_code);
+rust_code.slice(cidx,mt.index);
+cidx=mt.index+mt[0].length;
