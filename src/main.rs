3e5709bea40a511ad62d34babe69fe34d8d0c01b
mjz19910 <mjz19910@users.noreply.github.com>
2021-08-26 02:10:31 -0700

    YTFollow:catch up to 32:25
    https://youtu.be/Zkd3mZYOOvw?t=1945

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ enum Lang {
 #[derive(Clone)]
 enum Value {
 	Int(i64),
+	String(String),
 	Nothing,
 }
 
@@ -41,7 +42,19 @@ impl Evaluator {
 	}
 }
 
-fn parse_set(input:&[&str]) -> Result<Commands,EngineError> {
+fn parse_var_name(var_name) {
+	Ok(var_name.into())
+}
+
+fn parse_value(val:&str) -> Result<Value, EngineError>{
+	let result:Result<i64,_>=val.parse::<i64>();
+	match result {
+		Ok(x)=>return Value::Int(x),
+		_ => Err(EngineError::MismatchType),
+	}
+}
+
+fn parse_set(input:&[&str]) -> Result<Command,EngineError> {
 	if input.len() != 3 {
 		return Err(EngineError::MismatchNumParams)
 	}
@@ -49,19 +62,56 @@ fn parse_set(input:&[&str]) -> Result<Commands,EngineError> {
 	let var_name = parse_var_name(input[1])?
 
 	let value = parse_value(input[2])?
+
+	Ok(Command::SetVar(var_name, value))
 }
 
-fn parse_get(input:&[&str]) -> Result<Commands,EngineError> {
-	
+fn parse_get(input:&[&str]) -> Result<Command,EngineError> {
+	if input.len() != 2 {
+		return Err(EngineError::MismatchNumParams)
+	}
+
+	let var_name = parse_var_name(input[1])?
+
+	Ok(Command::GetVar(var_name))
 }
 
-parse_value(val) -> Result<Value, EngineError> {
+parse_string(val:&str)=>Result<Value,EngineError>{
+	if val.starts_with("\"") && val.ends_with("\"") && val.len() > 1 {
+		let inner = val[1..(val.len()-1)]
+	} else {
+		Err(EngineError::MismatchType)
+	}
+}
+
+parse_int(val:&str)=>Result<Value, EngineError>{
+	parse_i64(val)
+}
+
+parse_i64(val:&str)=>Result<Value, EngineError>{
 	let result = val.parse::<i64>::parse(val);
+
+	match result {
+		Ok(x) => Ok(Value::Int(x)),
+		_ => Err(EngineError::MismatchType),
+	}
 }
 
-fn parse(input: &str) -> Result<Vec<Commands>, EngineError> {
+parse_value(val:&str) -> Result<Value, EngineError> {
+	if val.starts_with("\"") && val.ends_with("\"") && val.len() > 1 {
+		// Parse the string
+		parse_string(val)
+	} else {
+		parse_int(val)
+	}
+}
+
+fn parse(input: &str) -> Result<Vec<Command>, EngineError> {
 	// set a 100
 	// get a
+
+	let mut output = vec![];
+
 	for line in input.lines() {
 		let command: Vec<_> = line.split_ascii_whitespace().collect();
         
@@ -72,12 +122,12 @@ fn parse(input: &str) -> Result<Vec<Commands>, EngineError> {
 			Some(x) if *x == "get" => {
 				output.push(parse_get(&command)?);
 			}
-			Some(x) => return Err(EngineError::UnkownCommand(name.to_string())),
+			Some(name) => return Err(EngineError::UnknownCommand(name.to_string())),
 			None => {}
 		}
 	}
 
-    Ok(vec![])
+    Ok(output)
 }
 
 #[test]
@@ -96,7 +146,32 @@ fn test1() -> Result<(), EngineError> {
 	Ok(())
 }
 
-/////#[test]
+#[test]
+fn parse_test1() -> Result<(), EngineError> {
+	let input = r#"set x "test"\nget x"#;
+
+	let commands = parse(input);
+
+	let mut evaluator=Evaluator::new();
+	let result = evaluator.evaluate(&commands)?;
+
+	assert_eq!(result, Value::Int(30));
+
+	Ok(())
+}
+
+fn parse_test2() -> Result<(), EngineError> {
+	let input = r#"set x "hello"\nget x"#;
+
+	let commands = parse(input);
+
+	let mut evaluator=Evaluator::new();
+	let result = evaluator.evaluate(&commands)?;
+
+	assert_eq!(result, Value::String("hello".into()));
+
+	Ok(())
+}
 
 fn main() {
 	println!("Hello World");
