200c5694586af20f1983493624ef8663479a5a5d
mjz19910 <mjz19910@users.noreply.github.com>
2021-08-29 10:45:35 -0700

    u

--- src/main.rs_to_js.js
+++ src/main.rs_to_js.js
@@ -368,8 +368,10 @@ x: {
 			}
 			let last_vec_info = __rust.scope.block_vec_stack.pop();
 			let block_vec_from_stack_id = __rust.scope.block_vec_ref.push(__rust.scope.block_vec);
+			let this_block_vec=__rust.scope.block_vec;
 			__rust.scope.block_vec = last_vec_info[1];
 			cur_block_id = last_vec_info[0];
+			debugger;
 			__rust.scope.block_vec.push([new BlockRef(__rust.scope, block_vec_from_stack_id - 1, cur_block_id)]);
 			__rust_priv.stack.length--;
 		}
