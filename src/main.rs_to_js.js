b2e251d063edf3ef778dd719b77eefca1d8a23b2
mjz19910 <mjz19910@users.noreply.github.com>
2021-08-28 17:44:18 -0700

    u

--- src/main.rs_to_js.js
+++ src/main.rs_to_js.js
@@ -1,5 +1,5 @@
 x: {
-	let rust_exec_code_funcs = ['STATIC_init','S_Crate_init',['rust_eval_struct','rust_eval_impl']];
+	let rust_exec_code_funcs = ['STATIC_init','S_Crate_init',['rust_eval_struct','rust_eval_impl','rust_eval_enum']];
 	let rr = function(mm, ...rest) {
 		for (i of rust_exec_code_funcs) {
 			for (let x of rest) {
@@ -584,16 +584,14 @@ x: {
 	}
 
 	${(function rust_eval_struct() {
-			__rust.exec_line(`
-/// Parsed token.
-/// It doesn't contain information about data that has been parsed,
-/// only the type of the token and its size.
-#[derive(Debug)]
-pub struct Token {
-	pub kind: TokenKind,
-	pub len: usize,
-}
-			`);
+			__rust.exec_line(`/// Parsed token.
+			/// It doesn't contain information about data that has been parsed,
+			/// only the type of the token and its size.
+			#[derive(Debug)]
+			pub struct Token {
+				pub kind: TokenKind,
+				pub len: usize,
+			}`);
 		})}
 	
 	impl Token {
@@ -602,6 +600,11 @@ pub struct Token {
 		}
 	}
 
+
+	${(function rust_eval_impl(){
+		'//nop;';
+	})}
+
 	${(function rust_eval_impl(){
 		__rust.exec_line(`
 		impl Token {
@@ -609,7 +612,6 @@ pub struct Token {
 				Token { kind, len }
 			}
 		}`);
-		__rust.log_lines(()=>console.log('here'));
 	})}
 	
 	/// Enum representing common lexeme types.
@@ -695,12 +697,103 @@ pub struct Token {
 		/// Unknown token, not expected by the lexer, e.g. "№"
 		Unknown,
 	}
+
+	${(function rust_eval_enum(){
+		__rust.exec_line(`/// Enum representing common lexeme types.
+		// perf note: Changing all \`usize\` to \`u32\` doesn't change performance. See #77629
+		#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+		pub enum TokenKind {
+			// Multi-char tokens:
+			/// "// comment"
+			LineComment { doc_style: Option<DocStyle> },
+			/// \`/* block comment */\`
+			///
+			/// Block comments can be recursive, so the sequence like \`/* /* */\`
+			/// will not be considered terminated and will result in a parsing error.
+			BlockComment { doc_style: Option<DocStyle>, terminated: bool },
+			/// Any whitespace characters sequence.
+			Whitespace,
+			/// "ident" or "continue"
+			/// At this step keywords are also considered identifiers.
+			Ident,
+			/// "r#ident"
+			RawIdent,
+			/// "12_u8", "1.0e-40", "b"123"". See \`LiteralKind\` for more details.
+			Literal { kind: LiteralKind, suffix_start: usize },
+			/// "'a"
+			Lifetime { starts_with_number: bool },
+		
+			// One-char tokens:
+			/// ";"
+			Semi,
+			/// ","
+			Comma,
+			/// "."
+			Dot,
+			/// "("
+			OpenParen,
+			/// ")"
+			CloseParen,
+			/// "{"
+			OpenBrace,
+			/// "}"
+			CloseBrace,
+			/// "["
+			OpenBracket,
+			/// "]"
+			CloseBracket,
+			/// "@"
+			At,
+			/// "#"
+			Pound,
+			/// "~"
+			Tilde,
+			/// "?"
+			Question,
+			/// ":"
+			Colon,
+			/// "$"
+			Dollar,
+			/// "="
+			Eq,
+			/// "!"
+			Bang,
+			/// "<"
+			Lt,
+			/// ">"
+			Gt,
+			/// "-"
+			Minus,
+			/// "&"
+			And,
+			/// "|"
+			Or,
+			/// "+"
+			Plus,
+			/// "*"
+			Star,
+			/// "/"
+			Slash,
+			/// "^"
+			Caret,
+			/// "%"
+			Percent,
+		
+			/// Unknown token, not expected by the lexer, e.g. "№"
+			Unknown,
+		}`);
+		__rust.log_lines(()=>console.log('here'));
+	})}
 	
 	#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
 	pub enum DocStyle {
 		Outer,
 		Inner,
 	}
+
+	${(function rust_eval_enum(){
+		'//nop;';
+	})}
 	
 	#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
 	pub enum LiteralKind {
