007ac7d4fd94c42e5ea0ee2384345757cb2f35ba
mjz19910 <mjz19910@users.noreply.github.com>
2021-08-28 20:33:12 -0700

    u

--- src/main.rs_to_js.js
+++ src/main.rs_to_js.js
@@ -256,6 +256,7 @@ x: {
 			__rust.block_vec=[];
 			block_id=0;
 			__rust_priv.stack.push({});
+			return ret;
 		}
 		__rust.drop=function(vv){
 			let last=__rust_priv.stack[__rust_priv.stack.length-1];
